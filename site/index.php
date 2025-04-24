840acb44942f076f64ae2f838df0dc8a5cbd1f37
Ilia Krupko <ikrupko@ourgapps.com>
2025-04-24 14:46:52 +0400

    second iteration

--- site/index.php
+++ site/index.php
@@ -6,6 +6,7 @@
 	<meta name="description" content="Flip - Learn flashcards">
 	<meta name="keywords" content="ios flashcards">
 	<link rel="canonical" href="https://flip.app/index.php">
+	<link rel="stylesheet" href="css/mycss.css">
 	<?php include("headers.html"); ?>
 </head>
 
@@ -22,11 +23,10 @@
 					<br>
 					<h1 class="title is-1 is-size-3-mobile">
 						Flip - Flashcards Reinvented
-						<p class="is-size-5" style="padding-top: 12px;"><span class="has-text-grey-dark">Learn new words easy<span class="has-text-info"> with time-spacing repetitions</span></p>
 					</h1>
 					<br>
 					<h3 class="subtitle is-3 is-size-5-mobile">
-						Master new words with Flip, a powerful flashcard app designed to optimize learning through time-spacing repetition.
+						Accelerate your language learning with Flip, a powerful flashcard app designed to optimize learning through time-spacing repetition.
 					</h3>
 					<div style="margin-top:24px;">
 						<a style="margin-right:24px; margin-top:24px;" href="https://apps.apple.com/app/flip-flashcards/id6670500654" class="button is-large is-responsive">
@@ -38,7 +38,7 @@
 					</div>
 				</div>
 				<div class="column level-item is-narrow has-text-centered">
-					<img style="height: 700px; margin-top: 16px; margin-left: 16px;" src="img/main.png" alt="Flip main screen">
+					<img style="height: 700px; margin-top: 16px; margin-left: 16px;" src="img/main1.png" alt="Flip main screen">
 				</div>
 			</div>
 		</div>
@@ -46,7 +46,7 @@
 
 	<hr>
 
-	<section class="why-signal section">
+<!-- 	<section class="why-signal section">
 		<div class="container">
 			<div class="columns is-centered">
 				<div class="column has-text-centered">
@@ -55,17 +55,22 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
-<!-- Why Flip so useful -->
+<!-- Why another flashcard app? -->
 	<section class="section">
 		<div class="container">
 			<h2 class="title is-3">
-				Why was Flip created?
+				Why <span class="flipcolor"><i>Another</i></span> Flashcard App?
 			</h2>
 			<div class="block is-size-5">
-				Hi, I’m Ilia, an indie developer passionate about language learning. For the past four years, I’ve used Memrise to learn foreign languages like French 🇫🇷, German 🇩🇪, and Italian 🇮🇹 (my Memrise profile). However, Memrise announced that it will stop supporting user-created dictionaries in the near future ❌. In response, I decided to create my own app called Flip, focused on learning - without the distractions of gamification or gimmicks.
-				<p>Flip is built with the same core learning philosophy that made Memrise great, but structured in a way that keeps all your knowledge in one place rather than splitting it into multiple thematic sets.</p>
+				Hi, I’m Ilia — an indie developer passionate about language learning 👋
+			</div>
+			<div class="block is-size-5">
+				For the past four years, I’ve used Memrise to learn foreign languages like French 🇫🇷, German 🇩🇪, and Italian 🇮🇹 (<a href="https://community-courses.memrise.com/user/kruiller46">my Memrise profile</a>). However, Memrise announced that it will stop supporting user-created dictionaries in the near future. In response, I decided to create my own app called Flip, focused on learning - without the distractions of gamification or gimmicks.
+			</div>
+			<div class="block is-size-5">
+				Flip is built with the same core learning philosophy that made Memrise great, but structured in a way that keeps all your knowledge in one place rather than splitting it into multiple thematic sets.
 			</div>
 		</div>
 	</section>
@@ -73,6 +78,12 @@
 	<!-- Features -->
 	<section class="section features">
 		<div class="container">
+			<h2 class="title is-3 ">
+                Why Flip is <span class="flipcolor">Awesome?</span>
+            </h2>
+            <div class="block is-size-5">
+                Flip is built with the same core learning philosophy that made Memrise great, but structured in a way that keeps all your knowledge in one place rather than splitting it into multiple thematic sets.
+            </div>
 			<div class="columns is-centered">
 				<div class="column is-half has-text-centered">
 					<div class="box">
@@ -83,9 +94,9 @@
 				</div>
 				<div class="column is-half has-text-centered">
 					<div class="box">
-						<h1 class="title is-1">🔊</h1>
-						<h4 class="title is-4">Automatic audio playback</h4>
-						<p>Flip automatically plays native-quality audio for every flashcard, so you can focus on pronunciation and listening skills without pressing a button.</p>
+						<h1 class="title is-1">🌟</h1>
+						<h4 class="title is-4">Spaced Repetition Algorithm</h4>
+						<p>Flip uses a proven spaced repetition system to show you cards just before you’re about to forget them – maximizing retention while minimizing study time. The algorithm adapts to your pace, helping you move words into longterm memory efficiently and naturally.</p>
 					</div>
 				</div>
 			</div>
@@ -100,10 +111,106 @@
 				<div class="column is-half has-text-centered">
 					<div class="box">
 						<h1 class="title is-1">📚</h1>
-						<h4 class="title is-4">Basic dicts</h4>
+						<h4 class="title is-4">Proofread Basic Set</h4>
 						<p>Flip includes built-in basic dictionaries for popular languages, covering the most common words and phrases you need to get started. Perfect for beginners or those building a solid foundation before diving deeper.</p>
 					</div>
 				</div>
+			</div>
+		</div>
+	</section>
+
+		<!-- Why Flip so useful -->
+    <section class="section">
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-offset-1">
+                	<p class="spaced">📍 Difficult cards marking</p>
+                	<p class="spaced">🔊 Automatic voiceover</p>
+                	<p class="spaced">🚫 Prevents duplicate entries in your dictionary</p>
+                	<p class="spaced">📈 Repetition plan to understand your learning load</p>
+                </div>
+                <div class="column">
+                	<p class="spaced">📋 Detailed analytics</p>
+                	<p class="spaced">🥷 100% anonymity, no registration</p>
+                    <p class="spaced">🌞 Works offline</p>
+                    <p class="spaced">🎉 <span class="has-text-danger">No Ads, No Subscriptions!</span></p>
+                </div>
+                <div class="column">
+                    <p class="spaced">🧘 No distractions or gamification</p>
+                    <p class="spaced">💾 Cloud autobackup</p>
+                    <p class="spaced">🌖 Light and dark themes</p>
+                    <p class="spaced">📦 App size is about 10Mb</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+	<!-- Pay Only For Result -->
+	<section class="section">
+		<div class="container">
+			<h2 class="title is-3">
+				Pay Only For <span class="flipcolor">Result</span>
+			</h2>
+			<div class="block is-size-5">
+				Flip is a results-based paid app. You are not paying for your time or the app itself, but only for the cards you have mastered.
+			</div>
+			<div class="block is-size-5">
+				At the start, Flip gives you 100 Premium cards for free. When a card reaches Level 5 (after 12 days period successful repetition) it begins to count toward your premium balance. Once your balance runs out, you’ll need to top up to keep learning cards. If Flip doesn’t help you memorize a card, you don’t pay.
+			</div>
+		</div>
+	</section>
+
+	<hr>
+
+	<!-- Screenshots -->
+    <section class="section">
+        <div class="container">
+            <h2 class="title is-3 ">
+                Screenshots
+            </h2>
+            <div class="columns is-size-6 has-text-grey">
+                <div class="column has-text-centered">
+                    <img src="img/screenshots/1.png" alt="Flip – Learn & Repeat screen">
+                    Learn & Repeat
+                </div>
+                <div class="column has-text-centered">
+                    <img src="img/screenshots/2.png" alt="Monetal expenses screen">
+                    Different Learning Methods
+                </div>
+                <div class="column has-text-centered">
+                    <img src="img/screenshots/3.png" alt="Monetal new transaction screen">
+                    Track progress
+                </div>
+                <div class="column has-text-centered">
+                    <img src="img/screenshots/4.png" alt="Monetal debts screen">
+                    One language – one set
+                </div>
+            </div>
+        </div>
+    </section>
+
+	<!-- Chrome extension? -->
+	<section class="section">
+		<div class="container">
+			<h2 class="title is-3">
+				Memrise To Flip Exporter
+			</h2>
+			<div class="block is-size-5">
+				There is a <a target="_blank" href="https://chromewebstore.google.com/detail/memrise-to-flip-exporter/bofgcbmkaekkiaojgnmegnfoeblmelcn">Chrome extension</a> I’ve also created to export all my Memrise community courses to Flip’s format. Right now, it converts the sets without any learning history, but if there’s enough interest, I could improve it to retain progress data in the future.
+			</div>
+			<img style="height: 168px" src="img/chromeExtension.webp" alt="Фотография разработчика">
+		</div>
+	</section>
+
+	<!-- Community -->
+	<section class="section">
+		<div class="container">
+			<h2 class="title is-3">
+				Community on Reddit 
+				<img style="height: 32px; vertical-align: middle; position: relative; top: -4px;" src="img/reddit.png" alt="Reddit icon">
+			</h2>
+			<div class="block is-size-5">
+				Stay connected with the Flip community on our dedicated <a target="_blank" href="https://www.reddit.com/r/flip_flashcards">Subreddit</a>! I regularly post behind-the-scenes updates, new feature announcements, and development insights. Whether you’re a language learner, a curious tester, or just want to share feedback — you’re warmly invited to join the conversation.
 			</div>
 		</div>
 	</section>
@@ -117,7 +224,7 @@
             <div class="columns level is-centered">
                 <div class="column has-text-right is-offset-1">
                     <div class="block">
-                        <i>"Hi, I’m Ilia, an indie developer passionate about language learning. For the past four years, I’ve used Memrise to learn foreign languages like French 🇫🇷, German 🇩🇪, and Italian 🇮🇹 (<a href="https://community-courses.memrise.com/user/kruiller46">my Memrise profile</a>). However, Memrise announced that it will stop supporting user-created dictionaries in the near future ❌. In response, I decided to create my own app called Flip, focused on learning - without the distractions of gamification or gimmicks."</i>
+                        <i>"Hi, I’m the creator of Flip. Since September 24, I’ve been using Flip daily to learn Italian and have already added over 3,500 words to my collection. I only wish I had built this app years ago - it has completely transformed my learning experience! I’m also passionate about building <a href="https://apps.apple.com/app/monetal-expense-tracker/id1302304930" target="_blank">simple and useful</a> mobile apps. If you have any questions, ideas, or feedback — feel free to reach out via <a href="mailto:support@monetal.app">email</a> or on <a target="_blank" href="https://www.reddit.com/r/flip_flashcards">Reddit</a>. I’d love to hear from you!"</i>
                     </div>
                     <div class="block has-text-grey-light">Ilia Krupko, developer of Flip</div>
                 </div>
